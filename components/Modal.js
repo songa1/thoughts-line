0acf42362ab0cfb98bda4a9b0512c8f57f9522d0
songa1 <songachillethe1her@gmail.com>
2021-08-24 12:43:54 +0200

    initial commit

---
+++ components/Modal.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+const Modal = (props) => {
+    const {isShown, setIsShown} = props;
+
+   
+  const closeModal = () => {
+    setIsShown(false);
+  };
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Modal;
