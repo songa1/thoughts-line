c66298bce5b4da211ee266124b5e726fd929b01f
songa1 <songachillethe1her@gmail.com>
2021-08-28 18:04:22 +0200

    Added google verification

--- pages/login.js
+++ pages/login.js
@@ -30,6 +30,7 @@ const Login = () => {
         <Head>
             <meta name="viewport" content="width=device-width,initial-scale=1.0"></meta>
             <meta name="robots" content="index, follow"></meta>
+            <meta name="google-site-verification" content="lzuO2DVXYYrKZZY1nLrFSPHa2AQkaY0pRWIPHJ9wjW4" />
             <title>Login to Achille Songa Blog</title>
         </Head>
         <div className='container'>
