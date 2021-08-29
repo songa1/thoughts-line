76f2d8122f0abea34d3bb546966ce60d1c3566f8
songa1 <songachillethe1her@gmail.com>
2021-08-29 15:36:54 +0200

    Added adsense code

--- pages/404.js
+++ pages/404.js
@@ -9,6 +9,7 @@ const NotFound = () => {
             <meta name="viewport" content="width=device-width,initial-scale=1.0"></meta>
             <meta name="robots" content="index, follow"></meta>
             <meta name="google-site-verification" content="lzuO2DVXYYrKZZY1nLrFSPHa2AQkaY0pRWIPHJ9wjW4" />
+            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3094763105861002" crossorigin="anonymous"></script>
             <title>Page Not Found | Achille Songa Blog</title>
         </Head>
         <div className='container'>
--- pages/about.js
+++ pages/about.js
@@ -8,6 +8,7 @@ const About = () => {
             <meta name="viewport" content="width=device-width,initial-scale=1.0"></meta>
             <meta name="robots" content="index, follow"></meta>
             <meta name="google-site-verification" content="lzuO2DVXYYrKZZY1nLrFSPHa2AQkaY0pRWIPHJ9wjW4" />
+            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3094763105861002" crossorigin="anonymous"></script>
             <title>About Achille Songa</title>
         </Head>
         <div className='container'>
