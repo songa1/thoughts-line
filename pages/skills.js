e2cf9f1769e5a7ced94d9db75175c2ea25453f9c
Cishahayo Songa Achille <41831989+songa1@users.noreply.github.com>
2022-06-30 01:22:55 -0700

    Update skills.js

--- pages/skills.js
+++ pages/skills.js
@@ -8,7 +8,6 @@ const Skills = () => {
         <Head>
             <meta name="viewport" content="width=device-width,initial-scale=1.0"></meta>
             <meta name="robots" content="index, follow"></meta>
-            <meta name="google-site-verification" content="lzuO2DVXYYrKZZY1nLrFSPHa2AQkaY0pRWIPHJ9wjW4" />
             <title>Skills of Achille Songa</title>
         </Head>
         <div className='container'>
