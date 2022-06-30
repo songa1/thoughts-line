7c181f0f6a85db90f323b138e492e51d8bc1d2a6
Cishahayo Songa Achille <41831989+songa1@users.noreply.github.com>
2022-06-30 01:18:47 -0700

    Update [id].js

--- pages/view/[id].js
+++ pages/view/[id].js
@@ -43,7 +43,6 @@ const Single =()=>{
         <Head>
             <meta name="viewport" content="width=device-width,initial-scale=1.0"></meta>
             <meta name="robots" content="index, follow"></meta>
-            <meta name="google-site-verification" content="lzuO2DVXYYrKZZY1nLrFSPHa2AQkaY0pRWIPHJ9wjW4" />
             <title>{post ? post.title : ''}</title>
         </Head>
         <div className='container'>
