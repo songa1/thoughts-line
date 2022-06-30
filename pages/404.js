93d50a1c8650751bbb810154cbc2b58037a4c234
Cishahayo Songa Achille <41831989+songa1@users.noreply.github.com>
2022-06-30 01:18:04 -0700

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -8,7 +8,6 @@ const NotFound = () => {
         <Head>
             <meta name="viewport" content="width=device-width,initial-scale=1.0"></meta>
             <meta name="robots" content="index, follow"></meta>
-            <meta name="google-site-verification" content="lzuO2DVXYYrKZZY1nLrFSPHa2AQkaY0pRWIPHJ9wjW4" />
             <title>Page Not Found | Achille Songa Blog</title>
         </Head>
         <div className='container'>
