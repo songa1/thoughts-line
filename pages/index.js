07317ef09f5fe2485a66108d250fcaee01530a9f
Cishahayo Songa Achille <41831989+songa1@users.noreply.github.com>
2022-06-30 01:16:34 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,6 @@ export default function Home() {
     <Head>
       <meta name="viewport" content="width=device-width,initial-scale=1.0"></meta>
       <meta name="robots" content="index, follow"></meta>
-      <meta name="google-site-verification" content="lzuO2DVXYYrKZZY1nLrFSPHa2AQkaY0pRWIPHJ9wjW4" />
       <title>Welcome to Achille Songa Blog</title>
     </Head>
     <div className='container'>
