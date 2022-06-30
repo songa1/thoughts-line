20aecb7ad0c73d33173278d319551fe7bf5ebf8a
Cishahayo Songa Achille <41831989+songa1@users.noreply.github.com>
2022-06-30 01:22:23 -0700

    Update projects.js

--- pages/projects.js
+++ pages/projects.js
@@ -7,7 +7,6 @@ const Projects = () => {
         <Head>
             <meta name="viewport" content="width=device-width,initial-scale=1.0"></meta>
             <meta name="robots" content="index, follow"></meta>
-            <meta name="google-site-verification" content="lzuO2DVXYYrKZZY1nLrFSPHa2AQkaY0pRWIPHJ9wjW4" />
             <title>Projects by Achille Songa</title>
         </Head>
         <div className='container'>
